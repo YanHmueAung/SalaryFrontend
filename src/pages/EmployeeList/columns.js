ffde19adc7e7b9002c4bf6952b5107f2b3cb5683
yan <yanhmueaung@gmail.com>
2022-05-02 20:34:17 +0800

    order the employee list by id ,name, login or salary by asc or des

--- src/pages/EmployeeList/columns.js
+++ src/pages/EmployeeList/columns.js
@@ -4,24 +4,28 @@ const columns = [
         dataIndex: "id",
         key: "id",
         width: 100,
+        sorter: (a, b) => a.id.localeCompare(b.id)
     },
     {
         title: "Name",
         dataIndex: "fullName",
         key: "fullName",
         width: 100,
+        sorter: (a, b) => a.fullName.localeCompare(b.fullName)
     },
     {
         title: "Login",
         dataIndex: "username",
         key: "username",
         width: 100,
+        sorter: (a, b) => a.username.localeCompare(b.username)
     },
     {
         title: "Salary",
         dataIndex: "salary",
         key: "salary",
         width: 100,
+        sorter: (a, b) => parseInt(a.salary) - parseInt(b.salary),
         render: (text) => 'S$' + text
     },
 ]
